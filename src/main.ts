9800eaa3000e5cc7dec86b3abcb8c2a4806578cc
SpairCode <1598111569@qq.com>
2024-11-23 19:38:23 +0800

    添加 route 、arco

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import ArcoVue from "@arco-design/web-vue";
+import App from "./App.vue";
+import "@arco-design/web-vue/dist/arco.css";
+import router from "./router/router";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(ArcoVue);
+app.use(router);
+app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+ import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    proxy: {
+      // 字符串简写写法
+      "/foo": "http://localhost:4567",
+      // 选项写法
+      "/api": {
+        target: "http://jsonplaceholder.typicode.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+      // 正则表达式写法
+      "^/fallback/.*": {
+        target: "http://jsonplaceholder.typicode.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/fallback/, ""),
+      },
+      // Proxying websockets or socket.io
+      "/socket.io": {
+        target: "ws://localhost:3000",
+        ws: true,
+      },
+    },
+  },
+});
